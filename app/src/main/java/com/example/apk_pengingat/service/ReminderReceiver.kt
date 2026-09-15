0146552a985bc31a1e12583ae76e0d23f74c4cbd
ariffandisaputra <ariffandisaputra@users.noreply.github.com>
2026-09-15 08:29:52 +0700

    fix: tambah import ReminderType di ReminderReceiver

--- app/src/main/java/com/example/apk_pengingat/service/ReminderReceiver.kt
+++ app/src/main/java/com/example/apk_pengingat/service/ReminderReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.example.apk_pengingat.data.db.AppDatabase
+import com.example.apk_pengingat.data.model.ReminderType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
